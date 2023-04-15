12d8717e8c4727e35149167b68070881f63b7101
Ivachev Konstantin <kostya1.6@yandex.ru>
2023-04-15 12:30:27 +0500

    Back1.0

--- controllers/users.js
+++ controllers/users.js
@@ -4,11 +4,18 @@ const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
+    .orFail()
     .then((newUser) => {
       res.send(newUser);
     })
     .catch((err) => {
-      res.status(400).send(err);
+      if (err.message === 'NotFound') {
+        res.status(404).send({ message: 'Карточка или пользователь не найден' });
+      } else if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные в методы создания карточки, пользователя, обновления аватара пользователя или профиля' });
+      } else {
+        res.status(500).send({ message: 'Что-то пошло не так...' });
+      }
     });
 };
 
@@ -17,11 +24,18 @@ const patchUser = (req, res) => {
   const { name, about } = req.body;
 
   User.updateOne({ _id: id }, { $set: { name, about } })
+    .orFail()
     .then((updateUser) => {
       res.send(updateUser);
     })
     .catch((err) => {
-      res.status(400).send(err);
+      if (err.message === 'NotFound') {
+        res.status(404).send({ message: 'Карточка или пользователь не найден' });
+      } else if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные в методы создания карточки, пользователя, обновления аватара пользователя или профиля' });
+      } else {
+        res.status(500).send({ message: 'Что-то пошло не так...' });
+      }
     });
 };
 
@@ -30,32 +44,53 @@ const patchAvatar = (req, res) => {
   const { avatar } = req.body;
 
   User.updateOne({ _id: id }, { $set: { avatar } })
+    .orFail()
     .then((updateUser) => {
       res.send(updateUser);
     })
     .catch((err) => {
-      res.status(400).send(err);
+      if (err.message === 'NotFound') {
+        res.status(404).send({ message: 'Карточка или пользователь не найден' });
+      } else if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные в методы создания карточки, пользователя, обновления аватара пользователя или профиля' });
+      } else {
+        res.status(500).send({ message: 'Что-то пошло не так...' });
+      }
     });
 };
 
 const getUser = (req, res) => {
   const { id } = req.params;
   User.findById(id)
+    .orFail()
     .then((user) => {
       res.send(user);
     })
     .catch((err) => {
-      res.status(400).send(err);
+      if (err.message === 'NotFound') {
+        res.status(404).send({ message: 'Карточка или пользователь не найден' });
+      } else if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные в методы создания карточки, пользователя, обновления аватара пользователя или профиля' });
+      } else {
+        res.status(500).send({ message: 'Что-то пошло не так...' });
+      }
     });
 };
 
 const getAllUser = (req, res) => {
   User.find()
+    .orFail()
     .then((users) => {
       res.send(users);
     })
     .catch((err) => {
-      res.status(400).send(err);
+      if (err.message === 'NotFound') {
+        res.status(404).send({ message: 'Карточка или пользователь не найден' });
+      } else if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные в методы создания карточки, пользователя, обновления аватара пользователя или профиля' });
+      } else {
+        res.status(500).send({ message: 'Что-то пошло не так...' });
+      }
     });
 };
 
