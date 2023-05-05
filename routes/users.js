583bf8ae4317ee9d4b9df92f78af1b8827a9e225
Ivachev Konstantin <kostya1.6@yandex.ru>
2023-05-05 13:07:02 +0500

    jwt3

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().min(2).max(30),
+    avatar: Joi.string().min(2).pattern(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
