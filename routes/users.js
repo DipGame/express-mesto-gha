ed1210a8bc9ca4ac3f3145f8bc6e79dd5855f257
Ivachev Konstantin <kostya1.6@yandex.ru>
2023-05-05 11:53:58 +0500

    jwt1

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,19 @@
 const userRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   createUser, getUser, getAllUser, patchUser, patchAvatar, login,
 } = require('../controllers/users');
 const Auth = require('../middlewares/auth');
 
-userRouter.post('/signup', createUser);
+userRouter.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), createUser);
 userRouter.post('/signin', login);
 userRouter.use('/users', Auth);
 userRouter.get('/users/:id', getUser);
